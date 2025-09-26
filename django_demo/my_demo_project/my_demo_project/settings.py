656a3b33a04c72e1fa2be6568cabca70a687fd23
Dylan Bosch <dylanbosch02@gmail.com>
2025-09-26 03:35:09 +0000

    Completed part 3 of Django app training. I created two models in the polls app (Question and Choice). I also regitered the Question model to the admin page and created an admin account.

--- django_demo/my_demo_project/my_demo_project/settings.py
+++ django_demo/my_demo_project/my_demo_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Custom applications below
+    'polls.apps.PollsConfig'
 ]
 
 MIDDLEWARE = [
