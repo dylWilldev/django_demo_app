4d496a149b74312eecf4c9455adf5e328b8c48c2
dylbos <dylanbosch02@gmail.com>
2025-10-07 21:27:57 -0600

    Updates gitignore, added a few more views and applicable urls for the polls application. Committing before addings templates to the polls application.

--- django_demo/my_demo_project/polls/urls.py
+++ django_demo/my_demo_project/polls/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    # /home_polls/
+    path("", views.home_index, name="index"),
+    # /polls/#/
+    path("<int:question_id>", views.detail, name="detail"),
+    # /polls/#/results
+    path("<int:question_id>/results", views.results, name="results"),
+    # /polls/#/vote
+    path("<int:question_id>/vote", views.vote, name="vote"),
 ]
--- django_demo/my_demo_project/polls/views.py
+++ django_demo/my_demo_project/polls/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello World! You are at the polls index.")
+def home_index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse(f"You are looking at question {question_id}")
+
+def results(request, question_id):
+    response = "You are looking at the results of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting on question {question_id}")
+    
